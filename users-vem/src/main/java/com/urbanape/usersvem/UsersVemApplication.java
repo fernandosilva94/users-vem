e91723df95592d2bd9be0c202d0183e2ac0a1c96
Fernando Silva <fernandoadmti@hotmail.com>
2022-12-26 18:34:54 -0300

    first commit

---
+++ users-vem/src/main/java/com/urbanape/usersvem/UsersVemApplication.java
@@ -0,0 +1,13 @@
+package com.urbanape.usersvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersVemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersVemApplication.class, args);
+	}
+
+}
---
+++ users-vem/src/test/java/com/urbanape/usersvem/UsersVemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbanape.usersvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersVemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
