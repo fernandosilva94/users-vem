21c23d8f2074b321529ef3a8dee783ef80de18e5
Fernando Silva <fernandoadmti@hotmail.com>
2022-12-27 22:17:30 -0300

    ajustes nos controllers de usuario e cartao

--- users-vem/src/main/java/com/urbanape/usersvem/controller/CartaoController.java
+++ users-vem/src/main/java/com/urbanape/usersvem/controller/CartaoController.java
@@ -1,10 +1,19 @@
 package com.urbanape.usersvem.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.urbanape.usersvem.services.CartaoService;
+
 @RestController
-@RequestMapping("/api/controller")
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/api/cartao")
 public class CartaoController {
-    
+
+    final CartaoService cartaoService;
+
+    public CartaoController(CartaoService cartaoService) {
+        this.cartaoService = cartaoService;
+    }
 }
--- users-vem/src/main/java/com/urbanape/usersvem/controller/UsuarioController.java
+++ users-vem/src/main/java/com/urbanape/usersvem/controller/UsuarioController.java
@@ -1,10 +1,21 @@
 package com.urbanape.usersvem.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.urbanape.usersvem.services.UsuarioService;
+
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api/usuario")
 public class UsuarioController {
+    
+    final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
     
 }
