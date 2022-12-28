0233a3718fad1070a1045fab9a3142a85d1da12b
Fernando Silva <fernandoadmti@hotmail.com>
2022-12-28 01:04:39 -0300

    criacao do metodo salvarUsuario do controller

--- users-vem/src/main/java/com/urbanape/usersvem/controller/UsuarioController.java
+++ users-vem/src/main/java/com/urbanape/usersvem/controller/UsuarioController.java
@@ -1,10 +1,19 @@
 package com.urbanape.usersvem.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.urbanape.usersvem.dtos.UsuarioDto;
+import com.urbanape.usersvem.model.UsuarioModel;
 import com.urbanape.usersvem.services.UsuarioService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -17,5 +26,10 @@ public class UsuarioController {
         this.usuarioService = usuarioService;
     }
 
-    
+    @PostMapping
+    public ResponseEntity<Object> salvarUsuario(@RequestBody @Valid UsuarioDto usuarioDto) {
+        var usuarioModel = new UsuarioModel();
+        BeanUtils.copyProperties(usuarioDto, usuarioModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.save(usuarioModel));
+    }
 }
