c0e74aff51ad8bd51069971ec44e20b68d4c0aa9
Fernando Silva <fernandoadmti@hotmail.com>
2023-01-24 15:02:07 -0300

    ajustes usuario

--- frontend/users-vem/src/app/app.component.ts
+++ frontend/users-vem/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { UsuarioService } from './usuario.service';
+import { Usuario } from './usuario';
+import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'users-vem';
+export class AppComponent implements OnInit {
+  public usuario!: Usuario[];
+
+  constructor(private usuarioService: UsuarioService) {}
+
+  ngOnInit() {
+    this.getUsuarios();
+  }
+
+  public getUsuarios(): void {
+    this.usuarioService.getUsuario().subscribe(
+      (response: Usuario[]) => {
+        this.usuario = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  }
 }
--- frontend/users-vem/src/app/usuario.service.ts
+++ frontend/users-vem/src/app/usuario.service.ts
@@ -2,12 +2,13 @@ import { Usuario } from './usuario';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
-  private apiServerUrl = '';
+  private apiServerUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) { }
 
