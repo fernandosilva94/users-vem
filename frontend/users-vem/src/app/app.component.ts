2c4de5f928ea6a5218e0dfb9af79278e3344fc84
Fernando Silva <fernandoadmti@hotmail.com>
2023-01-24 23:27:00 -0300

    ajustes no app.component

--- frontend/users-vem/src/app/app.component.ts
+++ frontend/users-vem/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { UsuarioService } from './usuario.service';
-import { Usuario } from './usuario';
+import { UsuarioService } from './services/usuario.service';
+import { UsuarioModel } from './usuario';
 import { Component, OnInit } from '@angular/core';
-import { HttpErrorResponse } from '@angular/common/http';
+// import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,22 +9,22 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public usuario!: Usuario[];
+  public usuario!: UsuarioModel[];
 
   constructor(private usuarioService: UsuarioService) {}
 
   ngOnInit() {
-    this.getUsuarios();
+    // this.getUsuarios();
   }
 
-  public getUsuarios(): void {
-    this.usuarioService.getUsuario().subscribe(
-      (response: Usuario[]) => {
-        this.usuario = response;
-      },
-      (error: HttpErrorResponse) => {
-        alert(error.message);
-      }
-    )
-  }
+  // public getUsuarios(): void {
+  //   this.usuarioService.getUsuario().subscribe(
+  //     (response: Usuario[]) => {
+  //       console.log(this.usuario = response);
+  //     },
+  //     (error: HttpErrorResponse) => {
+  //       alert(error.message);
+  //     }
+  //   )
+  // }
 }
--- frontend/users-vem/src/app/app.module.ts
+++ frontend/users-vem/src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { UsuarioService } from './usuario.service';
+import { UsuarioService } from './services/usuario.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { UsuarioComponent } from './usuario/usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule,
