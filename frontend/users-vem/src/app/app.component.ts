38f58c02994f36102e519313a17ac6154214d990
Fernando Silva <fernandoadmti@hotmail.com>
2023-01-25 01:18:19 -0300

    ajustes no appcomponent

--- frontend/users-vem/src/app/app.component.ts
+++ frontend/users-vem/src/app/app.component.ts
@@ -1,30 +1,9 @@
-import { UsuarioService } from './services/usuario.service';
-import { UsuarioModel } from './usuario';
-import { Component, OnInit } from '@angular/core';
-// import { HttpErrorResponse } from '@angular/common/http';
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  public usuario!: UsuarioModel[];
+export class AppComponent{
 
-  constructor(private usuarioService: UsuarioService) {}
-
-  ngOnInit() {
-    // this.getUsuarios();
-  }
-
-  // public getUsuarios(): void {
-  //   this.usuarioService.getUsuario().subscribe(
-  //     (response: Usuario[]) => {
-  //       console.log(this.usuario = response);
-  //     },
-  //     (error: HttpErrorResponse) => {
-  //       alert(error.message);
-  //     }
-  //   )
-  // }
 }
--- frontend/users-vem/src/app/app.module.ts
+++ frontend/users-vem/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UsuarioComponent } from './usuario/usuario.component';
+import { CartaoComponent } from './cartao/cartao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsuarioComponent
+    UsuarioComponent,
+    CartaoComponent
   ],
   imports: [
     BrowserModule,
