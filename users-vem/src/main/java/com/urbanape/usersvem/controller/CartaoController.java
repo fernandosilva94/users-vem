e1ae4978267a7e7c5c852c382ed1275667291ad7
Fernando Silva <fernandoadmti@hotmail.com>
2023-01-24 23:24:51 -0300

    ajustes nos controllers

--- users-vem/src/main/java/com/urbanape/usersvem/controller/CartaoController.java
+++ users-vem/src/main/java/com/urbanape/usersvem/controller/CartaoController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +22,6 @@ import com.urbanape.usersvem.services.CartaoService;
 import jakarta.validation.Valid;
 
 @RestController
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api/cartao")
 public class CartaoController {
 
