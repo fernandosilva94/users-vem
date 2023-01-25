beaac9146b7d01af07b66000c2de719575c8c65d
Fernando Silva <fernandoadmti@hotmail.com>
2023-01-25 01:16:52 -0300

    iniciando ajustes das rotas

--- frontend/users-vem/src/app/app-routing.module.ts
+++ frontend/users-vem/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { CartaoComponent } from './cartao/cartao.component';
+import { UsuarioComponent } from './usuario/usuario.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "usuario", component: UsuarioComponent},
+  {path: "cartao", component: CartaoComponent},
+  {path: "", redirectTo: "usuario", pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
