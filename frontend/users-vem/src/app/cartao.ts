d55e326023c4dbb038cf63b38399287736dcc66f
Fernando Silva <fernandoadmti@hotmail.com>
2023-01-30 20:36:36 -0300

    ajuste na interface do cartao

--- frontend/users-vem/src/app/cartao.ts
+++ frontend/users-vem/src/app/cartao.ts
@@ -1,9 +1,8 @@
-import { UsuarioModel } from './usuario';
 export interface CartaoModel {
   id: number;
   numeroCartao: number;
   nome: string;
   status: boolean;
   tipoCartao: string;
-  usuario: UsuarioModel;
+  usuarioId: number;
 }
