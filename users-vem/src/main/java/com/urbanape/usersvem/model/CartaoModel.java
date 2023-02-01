e3db4fcb96a846f7f348c1b199d856899d40eeb2
Fernando Silva <fernandoadmti@hotmail.com>
2023-02-01 15:08:18 -0300

    ajustes nas models backend

--- users-vem/src/main/java/com/urbanape/usersvem/model/CartaoModel.java
+++ users-vem/src/main/java/com/urbanape/usersvem/model/CartaoModel.java
@@ -35,7 +35,7 @@ public class CartaoModel implements Serializable {
     private String tipoCartao;
 
     @JsonBackReference
-    @ManyToOne(cascade=CascadeType.PERSIST)
+    @ManyToOne()
     @JoinColumn(name = "usuario_id")
     private UsuarioModel usuario;
 
--- users-vem/src/main/java/com/urbanape/usersvem/model/UsuarioModel.java
+++ users-vem/src/main/java/com/urbanape/usersvem/model/UsuarioModel.java
@@ -3,6 +3,7 @@ package com.urbanape.usersvem.model;
 import java.io.Serializable;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
